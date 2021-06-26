740d995f5977c061966df704cfa2a45f230c800a
Aly Fawzy <alyfawzy098@gmail.com>
2021-06-26 03:19:11 +0200

    remove unused files

--- Model/CharacterScreenModel/CharacterScreenModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CharacterScreenModel.swift
-//  BreakingBadCharacters
-//
-//  Created by ALY on 24/06/2021.
-//
-
-import Foundation
--- ViewModel/CharacterScreenViewModel/CharacterScreenViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CharacterScreenViewModel.swift
-//  BreakingBadCharacters
-//
-//  Created by ALY on 24/06/2021.
-//
-
-import Foundation
